2644b3e431c62270c467eb694cc60b4d97071687
Stanislas Nanchen <240862+stanislas@users.noreply.github.com>
2020-10-20 10:19:21 +0200

    alpha release status

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# http-client
+# Codesmith Http Client
+
+**This library is in ALPHA release; expect API breaking changes.**
+
 A Clojure Wrapper for the JRE 11 HttpClient
---
+++ test/codesmith/http/client_test.clj
@@ -0,0 +1,6 @@
+(ns codesmith.http.client-test
+  (:require [clojure.test :refer :all])
+  (:require [codesmith.http.client :refer [http-client]]))
+
+(deftest http-client-test
+  )
